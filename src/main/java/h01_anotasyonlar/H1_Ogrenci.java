355d69c4f0b2278a82c823e3dfbe0e252399380a
Murat Çakıroğlu <mcakiroglu@Murat-MBP.home>
2021-06-22 00:06:25 +0300

    day1

---
+++ src/main/java/h01_anotasyonlar/H1_Ogrenci.java
@@ -0,0 +1,86 @@
+package h01_anotasyonlar;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+//================================================================
+// HIBERNATE ICIN TABLO TANIMLAMASININ YAPILMASI 
+
+//POJO: Plain Old Java Object									
+//1) Private degiskenler tanimlanir.
+//2) constructor olusturulur.
+//3) Getter ve Setter metotlar tanimlanir.
+//4) toString() metodu ile nesne yazdirilabilir hale getirilir.
+//================================================================
+
+/*
+	Bir Class Hibernate ile tablo olusturmada kullanilacaksa degiskenlerin
+	"final" veya "static" tanimlanMAMAsi gerekir. 
+*/
+
+
+// - @Entity anotasyonu bu class'ın bir tabloya donusturulmesi
+//   gerektigini Hibernate'e gosterir. Her bir degisken bir sutun olacaktir.
+
+// - @Table annotasyonu tablo adinin degistirlmesini saglar.
+
+// - @Id anotasyonu tablo icerisinde primary key olusturur.
+
+// - @Transient anotasyonu bir degiskenin (sutun) tabloda yer almayacagini gosterir.
+
+@Entity
+@Table(name="ogrenci_tablosu")
+public class H1_Ogrenci {
+	
+	@Id
+	@Column(name="ogr_id")	
+	private int ogrId;
+	
+	@Column(name="ogr_ad")	
+	private String ogrAd;
+	
+	@Column(name="ogr_not")	
+	private int ogrNot;
+	
+	public H1_Ogrenci() {
+		
+	}
+	
+	public H1_Ogrenci(int ogrId, String ogrAd, int ogrNot) {
+		this.ogrId = ogrId;
+		this.ogrAd = ogrAd;
+		this.ogrNot = ogrNot;
+	}
+
+	public int getOgrId() {
+		return ogrId;
+	}
+
+	public void setOgrId(int ogrId) {
+		this.ogrId = ogrId;
+	}
+
+	public String getOgrAd() {
+		return ogrAd;
+	}
+
+	public void setOgrAd(String ogrAd) {
+		this.ogrAd = ogrAd;
+	}
+
+	public int getOgrNot() {
+		return ogrNot;
+	}
+
+	public void setOgrNot(int ogrNot) {
+		this.ogrNot = ogrNot;
+	}
+
+	@Override
+	public String toString() {
+		return "Ogrenci:  Id=" + ogrId + ", Ad=" + ogrAd + ", Not=" + ogrNot;
+	}
+	
+}
