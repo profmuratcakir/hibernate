006f85421679e0d4d64cef3ae6507d9d782d7fa7
Murat Çakıroğlu <mcakiroglu@Murat-MBP.home>
2021-06-24 00:15:50 +0300

    day3

--- src/main/java/h02_embeddable/H3_SaveFetch.java
+++ src/main/java/h02_embeddable/H3_SaveFetch.java
@@ -5,7 +5,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
-public class H3_Save {
+public class H3_SaveFetch {
 	
 	public static void main(String[] args) {
 		
@@ -29,9 +29,17 @@ public class H3_Save {
 		H1_Ogrenci ogrenci1 = new H1_Ogrenci(101, "Ahmet  Balta", 100, ders1 );
 		H1_Ogrenci ogrenci2 = new H1_Ogrenci(104, "Ahmet Yilmaz", 90, ders2 );
 				
-		session.save(ogrenci1);
-		session.save(ogrenci2);
-				
+		// Veritabanina kayit ekleme (Create - Update - Insert)
+//		session.save(ogrenci1);
+//		session.save(ogrenci2);
+		
+		// Fetch islemi (Veritabaninda veri okuma - READ)
+		H1_Ogrenci ogr1 = session.get(H1_Ogrenci.class, 102);
+		H1_Ogrenci ogr2 = session.get(H1_Ogrenci.class, 104);
+		
+		System.out.println("OGR1:" + ogr1 );  // Null dondurur.
+		System.out.println("OGR2:" + ogr2 );
+		
 		tx.commit();
 			
 		sf.close();
---
+++ src/main/java/h03_OnetoOneJoins/H4_Fetch.java
@@ -0,0 +1,64 @@
+package h03_OnetoOneJoins;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class H4_Fetch {
+
+	public static void main(String[] args) {
+		
+		Configuration con = new Configuration().configure("hibernate.cfg.xml")
+				.addAnnotatedClass(H1_Ogrenci.class)
+				.addAnnotatedClass(H2_Gunluk.class);
+		
+		SessionFactory sf = con.buildSessionFactory();
+		Session session = sf.openSession();
+	
+		Transaction tx = session.beginTransaction();
+		
+		H1_Ogrenci ogrenci = new H1_Ogrenci();
+		H2_Gunluk gunluk = new H2_Gunluk();
+		//id=101 olan ogrencinin bilgilerin get() metodu ile sorgulayalim.
+		ogrenci = session.get(H1_Ogrenci.class, 101);
+		System.out.println(ogrenci);
+				
+		//id=12 olan gunlugun bilgilerini getirelim.
+		gunluk = session.get(H2_Gunluk.class, 12);
+		System.out.println(gunluk);
+		
+		// Ogrenciler ve Gunlukler tablolarindaki ortak olan kayıtların,
+		// Ogrenci adi, gunluk yazisi(yazilar) ve ogrenci notu (ogrNot) bilgilerini sorgulayiniz.
+		
+		// SQL komutlari 
+		String sql1 = "SELECT o.ogr_adi, g.yazilar, o.ogrNot "
+					+ "FROM ogrenciler o INNER JOIN gunlukler g "
+					+ "ON o.ogr_id = g.ogr_id";
+		
+		List <Object[]> sonucListesi1 = session.createSQLQuery(sql1).getResultList();
+		
+		for(Object [] w: sonucListesi1) {
+			System.out.println(Arrays.toString(w));
+		}
+	
+		// HQL komutlari (Sadece Class ve degisken isimleri kullanimalidir. 
+		// Tablo tarafindaki isimlerdirmeler kullanilmaz)
+		String hql1 = "SELECT o.ogrAd, g.yazilar, o.ogrNot "
+					+ "FROM H1_Ogrenci o INNER JOIN H2_Gunluk g "
+					+ "ON o.ogrId = g.ogrenci";
+	
+		List<Object[]> sonucListesi2 = session.createQuery(hql1).getResultList();
+		for(Object [] w: sonucListesi2) {
+			System.out.println(Arrays.toString(w));
+		}
+		
+		tx.commit()	;
+		session.close();
+		sf.close();
+	}
+
+}
