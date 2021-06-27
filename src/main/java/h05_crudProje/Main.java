739ab174e1979ae0fec65ae27d50b53af983ae70
Murat Çakıroğlu <mcakiroglu@Murat-MBP.home>
2021-06-27 12:50:53 +0300

    day5

---
+++ src/main/java/h05_crudProje/Main.java
@@ -0,0 +1,29 @@
+package h05_crudProje;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//Crud islemleri icin gerekli olan class'dan nesnenin turetilmesi
+		CrudMetotlar metot = new CrudMetotlar();
+		
+		//Tum CRUD islemleri session factory olusturan metodu calistir. 
+		metot.sessionFactoryOlustur();
+		
+		//Veritabanina yeni personeller ekle
+//		metot.personelEkle("Ahmet","Yilmaz", 5500);
+//		metot.personelEkle("Mustafa","Baki", 7200);
+//		metot.personelEkle("Can","Ali", 4450);
+		
+		metot.idIlePersonelSil(32L);
+		metot.idIlePersonelSil(44L);
+		
+		metot.tumPersoneliListele();
+		metot.idIlePersonelListele(21L);
+		
+		metot.idIleMaasGuncelle(21L, 7500);
+		metot.tumPersoneliListele();
+		
+	}
+
+}
